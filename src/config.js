a7b5bbb3a852aacbb8cc8977630737b84d2cb9ad
madgallop <mgallop@middlebury.edu>
2023-10-11 10:10:02 -0400

    title, center and author
    getting set

--- src/config.js
+++ src/config.js
@@ -10,9 +10,9 @@ var config = {
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'The Title Text of this Story',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
+    title: 'Bread Riots in Lebanon',
+    subtitle: ': A Cartographic Analysis of Food Security in Lebanon',
+    byline: 'By Madeleine Gallop',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
@@ -23,7 +23,7 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: [-122.418398, 37.759483],
+                center: [35.81244, 33.87765],
                 zoom: 8.5,
                 pitch: 60,
                 bearing: 0
@@ -53,7 +53,7 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-77.020636, 38.886900],
+                center: [35.81244, 33.87765],
                 zoom: 8.5,
                 pitch: 60,
                 bearing: -43.2,
@@ -78,7 +78,7 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [6.15116, 46.20595],
+                center: [35.81244, 33.87765],
                 zoom: 12.52,
                 pitch: 8.01,
                 bearing: 0.00
@@ -97,7 +97,7 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-58.54195, -34.71600],
+                center: [35.81244, 33.87765],
                 zoom: 4,
                 pitch: 0,
                 bearing: 0
