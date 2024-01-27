7ae7576c28dce631619bbefdf3ccf38b20f68e76
madgallop <mgallop@middlebury.edu>
2024-01-27 11:06:46 -0500

    updates to bread riots
    and adding a .bib file!

--- docs/config.js
+++ docs/config.js
@@ -10,10 +10,10 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Bread Riots in Lebanon',
-    subtitle: ': A Cartographic Analysis of Food Security in Lebanon',
-    byline: 'By Madeleine Gallop',
-    introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.    ',
+    title: 'Cultivating Change: ',
+    subtitle: "Economic Distress, Unrest, and Unity in Lebanon since 1987",
+    byline: 'Madeleine Gallop',
+    introduction: "Lebanon has been facing a severe and enduring economic crisis since the late 1980s. These enduring economic troubles have reduced Lebanon’s ability to cultivate food and raised the prices of essential goods, resulting in food insecurity and a weakened agricultural system. While unrest has historically stemmed from sectarian affiliations, people protesting Lebanon’s economic conditions hail from each part of the country's divided geography. Lebanon must catch up to its population and overcome religious divisions to promote economic recovery and resilience in the agricultural sector to prevent further unrest.",
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
